18b4bdc07c02c22e779051018470d4b296080e5f
Sabastine <toshfuture@gmail.com>
2023-08-25 11:47:08 +0100

    Fixed Prisma userSubscription error

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -7,7 +7,7 @@ import { stripe } from '@/lib/stripe'
 
 export async function POST(req: Request) {
   const body = await req.text()
-  const signature = headers().get('stripe-signature') as string
+  const signature = headers().get('Stripe-Signature') as string
 
   let event: Stripe.Event
 
@@ -43,15 +43,23 @@ export async function POST(req: Request) {
   if (event.type === 'invoice.payment_succeeded') {
     const subscription = await stripe.subscriptions.retrieve(session.subscription as string)
 
-    await prismadb.userSubscription.update({
+    const userSubscription = await prismadb.userSubscription.findUnique({
       where: {
         stripeSubscriptionId: subscription.id,
       },
-      data: {
-        stripePriceId: subscription.items.data[0].price.id,
-        stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
-      },
     })
+
+    if (userSubscription) {
+      await prismadb.userSubscription.update({
+        where: {
+          stripeSubscriptionId: subscription.id,
+        },
+        data: {
+          stripePriceId: subscription.items.data[0].price.id,
+          stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
+        },
+      })
+    }
   }
 
   return new NextResponse(null, { status: 200 })
